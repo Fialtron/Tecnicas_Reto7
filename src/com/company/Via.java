1305b864da8dfa0765741a496cf9c59d1f98142f
Fialtron <moreno.juandi73@gmail.com>
2022-02-13 14:08:08 -0500

    Actualizacion clase Via
    Diferencia entre coordenadas iniciales y finales, atributo ancho y construccion de cuerpo en metodos area y longitud

--- src/com/company/Via.java
+++ src/com/company/Via.java
@@ -1,17 +1,17 @@
 package com.company;
 
 public abstract class Via {
-    double coordenadas[] = new double[2];
+    protected double coordenadasIniciales[] = new double[2];
+    protected double coordenadasFinales[] = new double[2];
+    protected int ancho ;
 
     public double longitud() {
-        return 1;
+        return Math.sqrt(Math.pow(2,coordenadasFinales[0]-coordenadasIniciales[0])-Math.pow(2,coordenadasFinales[1]-coordenadasIniciales[1]));
     }
 
     public double area() {
-        return 1;
+        return ancho*this.longitud();
     }
 
-    public double volumen() {
-        return 1;
-    }
+    public abstract double volumen();
 }
