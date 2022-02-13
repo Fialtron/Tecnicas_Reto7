ce247ec53b78f4b3955cb06bf9dc4e467483029a
Dina <dinamar582@hotmail.com>
2022-02-13 13:58:21 -0500

    logica asfalto

--- src/com/company/Asfalto.java
+++ src/com/company/Asfalto.java
@@ -1,7 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Asfalto extends Via{
     private String asfaltoProvedoor;
     private int velocidadMax;
     private boolean adelantar;
+
+    public Asfalto(String asfaltoProvedoor, int velocidadMax, boolean adelantar) {
+        super();
+        this.asfaltoProvedoor = asfaltoProvedoor;
+        this.velocidadMax = velocidadMax;
+        this.adelantar = adelantar;
+    }
+
+    @Override
+    public double volumen() {
+        return area() * 0.25;
+    }
 }
